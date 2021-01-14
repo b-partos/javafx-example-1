2a7564b4e920997b6679aa6d75b3189c41f86317
partosb <bal.partos@gmail.com>
2021-01-14 21:33:29 +0100

    Initial, non building state

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,31 @@
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class HelloWorld extends Application {
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+    public void start(Stage primaryStage) throws Exception {
+
+        primaryStage.setTitle("Hello World");
+
+        Group root = new Group();
+        Scene scene = new Scene(root, 300, 250);
+        Button btn = new Button();
+        btn.setLayoutX(100);
+        btn.setLayoutY(80);
+        btn.setText("Hello World");
+
+        btn.setOnAction(event -> System.out.println("Hello World"));
+
+        root.getChildren().add(btn);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+}
